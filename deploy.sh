015f2fba7bfcc8a1780eca246bf722e7efce54f2
Usama Ahmed <usama009lio@gmail.com>
2019-09-28 00:25:55 +0200

    removed whitespace

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,12 @@
 docker build -t uahmed9/multi-client:latest -t uahmed9/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t uahmed9/multi-server:latest -t uahmed9/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t uahmed9/multi-worker:latest -t uahmed9/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-
 docker push uahmed9/multi-client:latest
 docker push uahmed9/multi-server:latest
 docker push uahmed9/multi-worker:latest
-
 docker push uahmed9/multi-client:$SHA
 docker push uahmed9/multi-server:$SHA
 docker push uahmed9/multi-worker:$SHA
-
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=uahmed9/multi-server:$SHA
 kubectl set image deployments/client-deployment client=uahmed9/multi-client:$SHA
